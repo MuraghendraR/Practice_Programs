de8157a017f27ec362376e5a987e46fe644073e5
Muraghendra Ranjanagi <muraghendra.ranjanagi@gmail.com>
2023-03-04 21:12:16 +0530

    Sum of two numbers

---
+++ Practice_Code/src/com/practice/code/SumTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.practice.code;
+
+public class SumTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=10;
+		
+		int sum = a+b;
+		System.out.println("-->"+sum );
+		
+	}
+
+}
