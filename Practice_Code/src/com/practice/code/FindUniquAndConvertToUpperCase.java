4f2673652ee9c2cf877187f945eb6e8fd986620a
Muraghendra Ranjanagi <muraghendra.ranjanagi@gmail.com>
2023-03-05 10:41:32 +0530

    Finding Unique string and convering to upper case

---
+++ Practice_Code/src/com/practice/code/FindUniquAndConvertToUpperCase.java
@@ -0,0 +1,20 @@
+package com.practice.code;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindUniquAndConvertToUpperCase {
+	
+	public static void main(String[] args) {
+		List<String> str1 = Arrays.asList("a","b","c");
+		List<String> str2 = Arrays.asList("c","d","e");
+		
+		List<String> list = new ArrayList<>();
+		list.addAll(str1);
+		list.addAll(str2);
+		
+		list.stream().map(i->i.toUpperCase()).forEach(System.out::print);
+ 		
+	}
+}
